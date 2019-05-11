ace758464c4799d6a0f03df938bf12afa277cac5
ArturoBalleros <karveg7@gmail.com>
2019-05-12 01:43:30 +0200

    Comienzo diseño estimaciones

---
+++ ReadyReq/Controller/EstimateController.swift
@@ -0,0 +1,42 @@
+//
+//  EstimateController.swift
+//  ReadyReq
+//
+//  Created by Arturo Balleros Albillo on 12/05/2019.
+//  Copyright © 2019 Arturo Balleros Albillo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import NVActivityIndicatorView
+
+class EstimateController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Variables
+    
+
+    
+    // MARK: - View
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+    
+    // MARK: - TextFields
+ 
+    
+    // MARK: - Buttons
+    
+  
+    
+    // MARK: - Functions
+    
+  
+    
+    // MARK: - WebServices
+    
+  
+    
+  
+}
